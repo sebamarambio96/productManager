ea49358e6a9b37e5c5b7d4a2a653aefef9feb9ef
sebamarambio96 <s.marambiosandoval@gmail.com>
2023-08-09 23:42:43 -0400

    changeRole ahora verifica docus

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,11 +1,22 @@
+import { ErrorInvalidArgument } from "../models/errors/invalidArgument.js";
 import { usersRepository } from "../repositories/users.repository.js";
 import { usersService } from "../services/users.service.js";
+
 export async function changeRole(req, res, next) {
     try {
         const { uid } = req.params;
         //Verify existence of user
         const userData = await usersRepository.readOne({ _id: uid });
-        const newRole = userData.role === "premium" ? "user" : "premium";
+
+        let newRole = "";
+        if (userData.role === "premium") {
+            newRole = "user";
+        } else {
+            //Verify documents
+            if (userData.documents.length < 3) throw new ErrorInvalidArgument("El usuario no tiene los documentos necesarios");
+            newRole = "premium";
+        }
+
         usersService.changeRole(uid, newRole);
         res.status(200).json({ message: "Rol de usuario actualizado" });
     } catch (error) {
